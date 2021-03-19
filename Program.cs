06aed60acb1cf6e92f39585965dc236d32dd7a8e
Pruitt <apruitt89@outlook.com>
2021-03-19 03:00:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace RandomGuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(0, 100);
+
+            bool win = false;
+
+            do
+            {
+                Console.Write("Guess a number between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNum)
+                {
+                    Console.WriteLine("Oops too high!  Guess lower...");
+                }
+                else if (i < winNum)
+                {
+                    Console.WriteLine("Oops too low!  Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    Console.WriteLine("You Got It!!!");
+                    win = true;
+                }
+
+                Console.WriteLine();
+            } while (win == false);
+
+            Console.WriteLine("Thanks for playing!  Wink Wink...");
+            Console.WriteLine("Press any key to close:");
+            Console.ReadKey(true);
+        }
+    }
+}
